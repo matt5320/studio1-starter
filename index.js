7c65e42e0df0db27c97f547f44458021dfb06819
matt5320 <matt5320@mit.edu>
2025-03-21 16:02:07 -0400

    Finished

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
 import { createApp, Vue } from "https://mavue.mavo.io/mavue.js";
 
-let app = createApp({
-	data: {
-		tasks: [
-			{
-				done: false,
-				title: "",
-				active: true,
-			}
-		]
-	},
+let app = createApp(
+  {
+    data: {
+      tasks: [
+        {
+          done: false,
+          title: "",
+          active: true,
+        },
+      ],
+    },
 
-	methods: {
-		addItem() {
-			this.tasks.push({});
+    methods: {
+      addItem() {
+        for (const task of this.tasks) {
+          if (task.title === "") return;
+        }
+        this.tasks.push({
+          done: false,
+          title: "",
+          active: true,
+        });
 
-			this.setActive();
-		},
+        this.setActive();
+      },
 
-		deleteItem(i) {
-			this.tasks.splice(i, 1);
-			this.setActive(i - 1);
-		},
+      deleteItem(i) {
+        this.tasks.splice(i, 1);
+        this.setActive(i - 1);
+      },
 
-		clearCompleted () {
-			this.tasks = this.tasks.filter(task => !task.done);
-		},
+      clearCompleted() {
+        this.tasks = this.tasks.filter((task) => !task.done);
+      },
 
-		setActive(i) {
-			for (let task of this.tasks) {
-				task.active = false;
-			}
+      setActive(i) {
+        for (let task of this.tasks) {
+          task.active = false;
+        }
 
-			if (i >= 0 && this.tasks[i]) {
-				this.tasks[i].active = true;
-			}
-			else if (this.tasks.length > 0) {
-				this.tasks.at(-1).active = true;
-			}
-		},
-	}
-}, "#app");
+        if (i >= 0 && this.tasks[i]) {
+          this.tasks[i].active = true;
+        } else if (this.tasks.length > 0) {
+          this.tasks.at(-1).active = true;
+        }
+      },
+    },
+  },
+  "#app"
+);
 
 // For debugging
 globalThis.app = app;
